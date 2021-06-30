90171054e48c544f381e9b16077f4c8f944e461f
Tamara A <akira-49-q@yandex.ru>
2021-06-30 10:59:54 +0300

    feat: add drawer navigation

--- App.js
+++ App.js
@@ -9,13 +9,17 @@ import CategoriesScreen from './screens/CategoriesScreen';
 import CategoryMealScreen from './screens/CategoryMealScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
 import FavoritesScreen from './screens/FavoritesScreen';
+import FiltersScreen from './screens/FiltersScreen';
 import COLORS from './constants/colors';
 import StyledHeaderButton from './components/StyledHeaderButton';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
+const Drawer = createDrawerNavigator();
 const HomeStack = createStackNavigator();
 const FavStack = createStackNavigator();
+const FiltersStack = createStackNavigator();
 const Tab =
   Platform.OS === 'android'
     ? createMaterialBottomTabNavigator()
@@ -34,29 +38,47 @@ const tabNavigatorConfig =
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator {...tabNavigatorConfig}>
-        <Tab.Screen
-          name="Meals"
-          component={HomeTab}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="restaurant" size={25} color={color} />
-            ),
-            tabBarColor: COLORS.primaryColor,
-          }}
-        />
-        <Tab.Screen
-          name="Favorites"
-          component={FavTab}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="star" size={25} color={color} />
-            ),
-            tabBarColor: COLORS.accentColor,
-          }}
-        />
-      </Tab.Navigator>
+      <Drawer.Navigator
+        drawerContentOptions={{
+          activeTintColor:
+            Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
+          activeBackgroundColor:
+            Platform.OS === 'android' ? COLORS.primaryColor : 'white',
+          labelStyle: {
+            fontSize: 18,
+            fontFamily: 'OpenSans-Bold',
+            fontWeight: 'bold',
+          },
+        }}>
+        <Drawer.Screen name="Meals" component={TabNavigator} />
+        <Drawer.Screen name="Filters" component={FiltersNavigator} />
+      </Drawer.Navigator>
     </NavigationContainer>
+  );
+};
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator {...tabNavigatorConfig}>
+      <Tab.Screen
+        name="Meals"
+        component={HomeTab}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Icon name="restaurant" size={25} color={color} />
+          ),
+          tabBarColor: COLORS.primaryColor,
+        }}
+      />
+      <Tab.Screen
+        name="Favorites"
+        component={FavTab}
+        options={{
+          tabBarIcon: ({color}) => <Icon name="star" size={25} color={color} />,
+          tabBarColor: COLORS.accentColor,
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
@@ -74,7 +96,26 @@ const HomeTab = () => {
         headerTintColor:
           Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
       }}>
-      <HomeStack.Screen name="Categories" component={CategoriesScreen} />
+      <HomeStack.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={({navigation}) => ({
+          headerLeft: () => {
+            return (
+              <HeaderButtons HeaderButtonComponent={StyledHeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName="menu"
+                  onPress={() => {
+                    console.log('menu');
+                    navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            );
+          },
+        })}
+      />
       <HomeStack.Screen
         name="Category Meal"
         component={CategoryMealScreen}
@@ -99,7 +140,7 @@ const HomeTab = () => {
       <HomeStack.Screen
         name="Meal Detail"
         component={MealDetailScreen}
-        options={({route}) => ({
+        options={({route, navigation}) => ({
           title: route.params.meal.title,
           headerStyle: {
             backgroundColor:
@@ -118,13 +159,6 @@ const HomeTab = () => {
                   iconName="star"
                   onPress={() => {
                     console.log('fav');
-                  }}
-                />
-                <Item
-                  title="Unfavorite"
-                  iconName="star-outline"
-                  onPress={() => {
-                    console.log('unfav');
                   }}
                 />
               </HeaderButtons>
@@ -150,7 +184,26 @@ const FavTab = () => {
         headerTintColor:
           Platform.OS === 'android' ? 'white' : COLORS.accentColor,
       }}>
-      <FavStack.Screen name="Favorite Meals" component={FavoritesScreen} />
+      <FavStack.Screen
+        name="Favorite Meals"
+        component={FavoritesScreen}
+        options={({navigation}) => ({
+          headerLeft: () => {
+            return (
+              <HeaderButtons HeaderButtonComponent={StyledHeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName="menu"
+                  onPress={() => {
+                    console.log('menu');
+                    navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            );
+          },
+        })}
+      />
       <FavStack.Screen
         name="Meal Detail"
         component={MealDetailScreen}
@@ -188,6 +241,42 @@ const FavTab = () => {
         })}
       />
     </FavStack.Navigator>
+  );
+};
+
+const FiltersNavigator = () => {
+  return (
+    <FiltersStack.Navigator>
+      <FiltersStack.Screen
+        name="Filter Meals"
+        component={FiltersScreen}
+        options={({navigation}) => ({
+          headerStyle: {
+            backgroundColor:
+              Platform.OS === 'android' ? COLORS.primaryColor : 'white',
+          },
+          headerTitleStyle: {
+            color: Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
+          },
+          headerTintColor:
+            Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
+          headerLeft: () => {
+            return (
+              <HeaderButtons HeaderButtonComponent={StyledHeaderButton}>
+                <Item
+                  title="Menu"
+                  iconName="menu"
+                  onPress={() => {
+                    console.log('menu');
+                    navigation.toggleDrawer();
+                  }}
+                />
+              </HeaderButtons>
+            );
+          },
+        })}
+      />
+    </FiltersStack.Navigator>
   );
 };
 
