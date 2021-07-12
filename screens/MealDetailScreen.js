2c6e0e3ce770490d7709c20118129ec4ffa773b8
Tamara A <akira-49-q@yandex.ru>
2021-07-12 20:48:30 +0300

    feat: add Meal Detail Screen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,32 +1,79 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import {StyleSheet, View, Image, ScrollView} from 'react-native';
 import {useRoute} from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import RegularText from '../components/RegularText';
+import BoldText from '../components/BoldText';
+
+const ListItem = ({children}) => {
+  return (
+    <View style={styles.listItem}>
+      <RegularText>{children}</RegularText>
+    </View>
+  );
+};
 
 const MealDetailScreen = () => {
   const route = useRoute();
   const meal = route.params.meal;
 
   return (
-    <View style={styles.screen}>
-      <Text>Meal Detail Screen</Text>
-      <Text>{meal.title}</Text>
-      <Text>{meal.compleity}</Text>
-      <Text>{meal.isVegan ? 'is vegan' : 'not vegan'}</Text>
-      <Text>{meal.affordability}</Text>
-      <Icon name="star-outline" size={30} />
-      <Button title="star-outline" labelStyle={{fontSize: 25}}>
-        <Icon name="star-outline" size={23} style={{fontSize: 30}} />
-      </Button>
-    </View>
+    <ScrollView style={styles.screen}>
+      <View style={styles.wrapper}>
+        <Image source={{uri: meal.imageUrl}} style={styles.image} />
+        <View style={styles.detailsWrapper}>
+          <RegularText>{meal.duration} min</RegularText>
+          <RegularText>{meal.compleity.toUpperCase()}</RegularText>
+          <RegularText>{meal.affordability.toUpperCase()}</RegularText>
+        </View>
+        <View style={styles.block}>
+          <BoldText style={styles.blockTitle}>INGRIDIENTS</BoldText>
+          {meal.ingredients.map(item => (
+            <ListItem key={item}>{item}</ListItem>
+          ))}
+        </View>
+        <View style={styles.block}>
+          <BoldText style={styles.blockTitle}>STEPS</BoldText>
+          {meal.steps.map(item => (
+            <ListItem key={item}>{item}</ListItem>
+          ))}
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  detailsWrapper: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    paddingHorizontal: 15,
+    paddingTop: 10,
+  },
+  block: {
+    padding: 10,
+  },
+  blockTitle: {
+    fontSize: 18,
+    paddingBottom: 5,
+    textAlign: 'center',
+  },
+  listItem: {
+    margin: 5,
+    padding: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
   },
 });
 
