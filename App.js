81ed4cff9ef67db1ba54993ad5dce393ef40309d
Tamara A <akira-49-q@yandex.ru>
2021-07-15 10:25:04 +0300

    feat: add redax store

--- App.js
+++ App.js
@@ -9,27 +9,38 @@ import {createDrawerNavigator} from '@react-navigation/drawer';
 import TabNavigator from './navigation/TabNavigator';
 import FiltersNavigator from './navigation/FilterNavigator';
 
+import {Provider} from 'react-redux';
+import {createStore, combineReducers} from 'redux';
+import mealsReducer from './store/reducers/meals';
+
 const Drawer = createDrawerNavigator();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Drawer.Navigator
-        drawerContentOptions={{
-          activeTintColor:
-            Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
-          activeBackgroundColor:
-            Platform.OS === 'android' ? COLORS.primaryColor : 'white',
-          labelStyle: {
-            fontSize: 18,
-            fontFamily: 'OpenSans-Bold',
-            fontWeight: 'bold',
-          },
-        }}>
-        <Drawer.Screen name="Meals" component={TabNavigator} />
-        <Drawer.Screen name="Filters" component={FiltersNavigator} />
-      </Drawer.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Drawer.Navigator
+          drawerContentOptions={{
+            activeTintColor:
+              Platform.OS === 'android' ? 'white' : COLORS.primaryColor,
+            activeBackgroundColor:
+              Platform.OS === 'android' ? COLORS.primaryColor : 'white',
+            labelStyle: {
+              fontSize: 18,
+              fontFamily: 'OpenSans-Bold',
+              fontWeight: 'bold',
+            },
+          }}>
+          <Drawer.Screen name="Meals" component={TabNavigator} />
+          <Drawer.Screen name="Filters" component={FiltersNavigator} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
