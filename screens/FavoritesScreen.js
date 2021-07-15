7fad5c32110528b28db05f5c9f0de3c20b9a8840
Tamara A <akira-49-q@yandex.ru>
2021-07-15 10:44:55 +0300

    feat: rendering a fallback text

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,13 +2,21 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import MealsList from '../components/MealsList';
 import {useSelector} from 'react-redux';
+import BoldText from '../components/BoldText';
 
 const FavoritesScreen = ({navigation}) => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
 
   return (
     <View style={styles.screen}>
-      <MealsList dataList={favMeals} navigation={navigation} />
+      {favMeals.length === 0 || !favMeals ? (
+        <View style={styles.wrapper}>
+          <BoldText style={styles.text}>No favorite meals found.</BoldText>
+          <BoldText style={styles.text}>Start adding some!</BoldText>
+        </View>
+      ) : (
+        <MealsList dataList={favMeals} navigation={navigation} />
+      )}
     </View>
   );
 };
@@ -18,6 +26,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  wrapper: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
   },
 });
 
